fecd7250dc9b430a4ffacd002fe053eb8ac66bd8
Shin-sibainu <shincode0712@gmail.com>
2024-05-20 10:26:48 +0900

    delete post

--- components/post-operations.tsx
+++ components/post-operations.tsx
@@ -22,6 +22,29 @@ import {
 } from "./ui/alert-dialog";
 import { useRouter } from "next/navigation";
 import { useState } from "react";
+import { toast } from "./ui/use-toast";
+
+async function deletePost(postId: string) {
+  try {
+    const response = await fetch(`/api/posts/${postId}`, {
+      method: "DELETE",
+    });
+
+    if (!response.ok) {
+      throw new Error("Failed to delete the post.");
+    }
+
+    return true;
+  } catch (error) {
+    toast({
+      title: "Something went wrong.",
+      description: "Your post was not deleted. Please try again.",
+      variant: "destructive",
+    });
+
+    return false; // エラーが発生した場合は false を返す
+  }
+}
 
 interface PostOperationsProps {
   post: Pick<Post, "id" | "title">;
@@ -67,7 +90,16 @@ export default function PostOperations({ post }: PostOperationsProps) {
               onClick={async (event) => {
                 event.preventDefault();
                 setIsDeleteLoading(true);
+
+                const deleted = await deletePost(post.id);
+
+                if (deleted) {
+                  setIsDeleteLoading(false);
+                  setShowDeleteAlert(false);
+                  router.refresh();
+                }
               }}
+              className="bg-red-600 focus:ring-red-600"
             >
               {isDeleteLoading ? (
                 <Icons.spinner className="mr-2 h-4 w-4 animate-spin" />
